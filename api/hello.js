f6a6b1563f61c24ec5aa35423293c4705c3744f2
안민혁 <anminhyeog485@gmail.com>
2025-05-26 22:41:31 +0900

    Initial commit

--- api/hello.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "✅ 디버깅용 응답!",
-    SERVICE_KEY: process.env.SERVICE_KEY || "없음",
-    areaCode: req.query.areaCode || "없음",
-  });
-}
